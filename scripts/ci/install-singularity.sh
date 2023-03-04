06ebb04387375e196b30ea2abfb2f55e6e836491
Yaroslav Halchenko <debian@onerussian.com>
2023-03-04 09:53:45 -0500

    Just make install-singularity fail as soon as any command fails
    for some reason failed to download on jammy as release was not known but proceeded to
    testing
    
    https://github.com/datalad/datalad-extensions/actions/runs/4328883561/jobs/7558995329
    
    Lets see if ok now

--- scripts/ci/install-singularity.sh
+++ scripts/ci/install-singularity.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+set -eu
+
 release="$(curl -fsSL https://api.github.com/repos/sylabs/singularity/releases/latest | jq -r .tag_name)"
 codename="$(lsb_release -cs)"
 arch="$(dpkg --print-architecture)"
-wget -O /tmp/singularity-ce.deb https://github.com/sylabs/singularity/releases/download/$release/singularity-ce_${release#v}-${codename}_$arch.deb
+wget -O /tmp/singularity-ce.deb "https://github.com/sylabs/singularity/releases/download/$release/singularity-ce_${release#v}-${codename}_$arch.deb"
 sudo dpkg -i /tmp/singularity-ce.deb
 sudo apt-get install -f
