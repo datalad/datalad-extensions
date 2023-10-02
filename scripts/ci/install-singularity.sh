eab4897574292f3b40f96c9cb24fd2354034dff4
Yaroslav Halchenko <debian@onerussian.com>
2023-10-02 15:01:26 -0400

    Copy singularity installation script from datalad-container 1.2.2-17-g2de85e0  and also install libfuse2 for singularity
    My bad of two changes in one commit... shit happens
    
    Since ran into following on datalad-extensions:
    https://github.com/datalad/datalad-extensions/actions/runs/6375049463/job/17300367091
    
        Selecting previously unselected package singularity-ce.
        (Reading database ... 230073 files and directories currently installed.)
        Preparing to unpack /tmp/singularity-ce.deb ...
        Unpacking singularity-ce (4.0.0-jammy) ...
        dpkg: dependency problems prevent configuration of singularity-ce:
         singularity-ce depends on libfuse2 (>= 2.6); however:
        Processing triggers for man-db (2.10.2-1) ...
          Package libfuse2 is not installed.
    
        dpkg: error processing package singularity-ce (--install):
         dependency problems - leaving unconfigured
        Errors were encountered while processing:
         singularity-ce

--- scripts/ci/install-singularity.sh
+++ scripts/ci/install-singularity.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-
-set -eu
-
+set -ex -o pipefail
 release="$(curl -fsSL https://api.github.com/repos/sylabs/singularity/releases/latest | jq -r .tag_name)"
 codename="$(lsb_release -cs)"
 arch="$(dpkg --print-architecture)"
-wget -O /tmp/singularity-ce.deb "https://github.com/sylabs/singularity/releases/download/$release/singularity-ce_${release#v}-${codename}_$arch.deb"
+wget -O /tmp/singularity-ce.deb https://github.com/sylabs/singularity/releases/download/$release/singularity-ce_${release#v}-${codename}_$arch.deb
+set -x
+sudo apt-get install uidmap libfuse2
 sudo dpkg -i /tmp/singularity-ce.deb
 sudo apt-get install -f
